f55bf30f3f99bc08e894b36beba74b808219894a
douglas.bullard@gmail.com <douglas.bullard@gmail.com@3cc4c3fc-e03d-11dd-ab27-75d9513d8d6c>
2012-06-28 23:41:59 +0000

    refactoring

--- trunk/src/test/java/com/nurflugel/buildtasks/todo/TodosTest.java
+++ trunk/src/test/java/com/nurflugel/buildtasks/todo/TodosTest.java
@@ -11,6 +11,7 @@ import static com.nurflugel.buildtasks.todo.User.ALL;
 import static com.nurflugel.buildtasks.todo.User.UNKNOWN;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
 
 /** Tests for the FindTodosTask task. */
 @Test(groups = "unit")
@@ -27,7 +28,7 @@ public class TodosTest
     task.setReportDir(new File("build/reports"));
     task.setNamePattern("dbulla(dgb;doug),snara(snara3;sunitha),bren");
 
-    List<User> users = task.parseUsers();
+    List<User> users = null;  // task.parseUsers();
 
     task.findTodos(dir, users);
 
@@ -45,84 +46,115 @@ public class TodosTest
     assertEquals(todos.size(), 1);
   }
 
-  @Test(groups             = "unit",
-        expectedExceptions = BuildException.class)
+  @Test(expectedExceptions = BuildException.class)
   public void testParseBadUsers1()
   {
     FindTodosTask task = new FindTodosTask();
 
     task.setNamePattern("dbulla{dgb;doug;dbulla),snara(snara;sunitha)");
-    task.parseUsers();
+
+    // task.parseUsers();
   }
 
-  @Test(groups             = "unit",
-        expectedExceptions = BuildException.class)
+  @Test(expectedExceptions = BuildException.class)
   public void testParseBadUsers2()
   {
     FindTodosTask task = new FindTodosTask();
 
     task.setNamePattern("dbulla(dgb;doug;dbulla]:snara(snara;sunitha)");
-    task.parseUsers();
+
+    // task.parseUsers();
   }
 
-  @Test(groups = "unit")
+  @Test
   public void testParseEmptyUsers()
   {
     FindTodosTask task = new FindTodosTask();
 
     task.setNamePattern("");
-
-    List<User> users = task.parseUsers();
-
-    assertEquals(users.size(), 2);  // all and unknown
-    task.setNamePattern("");
-    users = task.parseUsers();
-    assertEquals(users.size(), 2);
-    task.setNamePattern(null);
-    users = task.parseUsers();
-    assertEquals(users.size(), 2);
+    fail();
+    // List<NameWithAliases> users = task.parseUsers();
+    //
+    // assertEquals(users.size(), 2);  // all and unknown
+    // task.setNamePattern("");
+    // users = task.parseUsers();
+    // assertEquals(users.size(), 2);
+    // task.setNamePattern(null);
+    // users = task.parseUsers();
+    // assertEquals(users.size(), 2);
   }
 
-  @Test(groups = "unit")
+  @Test
   public void testParseGoodUsers()
   {
     FindTodosTask task = new FindTodosTask();
 
     task.setNamePattern("dbulla(dgb;doug),snara(sunitha),bob");
+    fail();
+    // List<NameWithAliases> users = task.parseUsers();
+    //
+    // assertEquals(users.size(), 5);
+    //
+    // NameWithAliases all     = users.get(0);
+    // NameWithAliases unknown = users.get(1);
+    //
+    // assertEquals(all.getName(), ALL);
+    // assertTrue(all.getAliases().isEmpty());
+    // assertEquals(unknown.getName(), UNKNOWN);
+    //
+    // NameWithAliases dbulla = users.get(2);
+    //
+    // assertEquals(dbulla.getName(), "dbulla");
+    //
+    // Set<String> aliases = dbulla.getAliases();
+    //
+    // assertTrue(aliases.contains("dbulla"));
+    // assertTrue(aliases.contains("doug"));
+    // assertTrue(aliases.contains("dgb"));
+    //
+    // NameWithAliases snara = users.get(3);
+    //
+    // assertEquals(snara.getName(), "snara");
+    // aliases = snara.getAliases();
+    // assertTrue(aliases.contains("snara"));
+    // assertTrue(aliases.contains("sunitha"));
+    //
+    // NameWithAliases bob = users.get(4);
+    //
+    // assertEquals(bob.getName(), "bob");
+    // aliases = bob.getAliases();
+    // assertTrue(aliases.contains("bob"));
+  }
 
-    List<User> users = task.parseUsers();
+  @Test
+  public void testDefaultSearchPhrase()
+  {
+    FindTodosTask task = new FindTodosTask();
 
-    assertEquals(users.size(), 5);
+    assertEquals(task.getSearchPhrase(), "todo");
+  }
 
-    User all     = users.get(0);
-    User unknown = users.get(1);
+  /** Similar to the multiple user aliases with a single name, search for mulitiple variants of a phrase. */
+  @Test
+  public void testMultipleValueSearchPhrase() throws IOException
+  {
+    FindTodosTask task = new FindTodosTask();
 
-    assertEquals(all.getId(), ALL);
-    assertTrue(all.getAliases().isEmpty());
-    assertEquals(unknown.getId(), UNKNOWN);
+    task.setSearchPhrase("codereview(Code Review;code_review)");
 
-    User dbulla = users.get(2);
+    File dir = new File(getFilePath("dir"));
 
-    assertEquals(dbulla.getId(), "dbulla");
-
-    Set<String> aliases = dbulla.getAliases();
-
-    assertTrue(aliases.contains("dbulla"));
-    assertTrue(aliases.contains("doug"));
-    assertTrue(aliases.contains("dgb"));
-
-    User snara = users.get(3);
-
-    assertEquals(snara.getId(), "snara");
-    aliases = snara.getAliases();
-    assertTrue(aliases.contains("snara"));
-    assertTrue(aliases.contains("sunitha"));
-
-    User bob = users.get(4);
-
-    assertEquals(bob.getId(), "bob");
-    aliases = bob.getAliases();
-    assertTrue(aliases.contains("bob"));
+    task.setBaseDir(dir);
+    task.setReportDir(new File("build/reports"));
+    task.setNamePattern("dbulla(dgb;doug),snara(snara3;sunitha),bren");
+    fail();
+    // List<NameWithAliases> users = task.parseUsers();
+    //
+    // task.findTodos(dir, users);
+    // User user = users.get(0);
+    // assertEquals(user.getName(),"dbulla");
+    // List<TodoItem> todos = user.getTodos();
+    // assertEquals(todos,3);
   }
 
   // todo validate todos searched for if searchphrase not used
--- trunk/src/test/resources/dir/otherDir/Comments.java
+++ trunk/src/test/resources/dir/otherDir/Comments.java
@@ -33,5 +33,8 @@ public class Comments
    * todo dbulla
    * todo dbulla
    *
+   * codereview dbulla
+   * code review dbulla
+   * code_review dbulla
    */
 }
