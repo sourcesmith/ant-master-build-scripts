3275dc5fe82248d1a4d104a2c28d2c9ff9cbd278
douglas.bullard@gmail.com <douglas.bullard@gmail.com@3cc4c3fc-e03d-11dd-ab27-75d9513d8d6c>
2012-07-01 00:50:21 +0000

    Increasing test coverage

--- trunk/src/test/java/com/nurflugel/buildtasks/todo/LineSplitterTest.java
+++ trunk/src/test/java/com/nurflugel/buildtasks/todo/LineSplitterTest.java
@@ -1,10 +1,14 @@
 package com.nurflugel.buildtasks.todo;
 
 import org.apache.tools.ant.BuildException;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import static com.nurflugel.buildtasks.todo.LineSplitter.splitLine;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 /** Created with IntelliJ IDEA. User: dbulla Date: 6/28/12 Time: 4:19 PM To change this template use File | Settings | File Templates. */
+@Test(groups = "todos")
 public class LineSplitterTest
 {
   @Test
@@ -12,12 +16,15 @@ public class LineSplitterTest
   {
     String[] strings = splitLine("dbulla(dgb;douglas;doug),dlabar(derek),dduddl(dave;david)");
 
-    for (String string : strings)
+    for (String text : strings)
     {
-      System.out.println("string = " + string);
+      System.out.println("string = " + text);
     }
 
-    // assertEquals(strings.length, 3);  // should have dbulla, dlabar, dduddl
+    assertEquals(strings.length, 3);  // should have dbulla, dlabar, dduddl
+    assertTrue(strings[0].startsWith("dbulla"));
+    assertTrue(strings[1].startsWith("dlabar"));
+    assertTrue(strings[2].startsWith("dduddl"));
   }
 
   @Test(expectedExceptions = BuildException.class)
@@ -42,5 +49,23 @@ public class LineSplitterTest
   public void testBadChars4()
   {
     splitLine("dbulla(dgb;douglas;doug},dlabar(derek),dduddl(dave;david)");
+  }
+
+  @Test
+  public void testNoNames()
+  {
+    String   namePattern = "didkdksksks slkjlskjslks";
+    String[] strings     = splitLine(namePattern);
+
+    assertEquals(strings.length, 1);
+    assertEquals(strings[0], namePattern);
+  }
+
+  @Test
+  public void testNoText()
+  {
+    String[] strings = splitLine("");
+
+    assertEquals(strings.length, 0);
   }
 }
