f55135a5439615b0c5671e6f43fd5bdf979de370
douglas.bullard@gmail.com <douglas.bullard@gmail.com@3cc4c3fc-e03d-11dd-ab27-75d9513d8d6c>
2012-07-05 21:57:41 +0000

    added new class and tests

---
+++ trunk/src/main/java/com/nurflugel/buildtasks/todo/BadParsingException.java
@@ -0,0 +1,10 @@
+package com.nurflugel.buildtasks.todo;
+
+/** Created with IntelliJ IDEA. User: dbulla Date: 7/5/12 Time: 2:51 PM To change this template use File | Settings | File Templates. */
+public class BadParsingException extends Exception
+{
+  public BadParsingException(String message)
+  {
+    super(message);
+  }
+}
