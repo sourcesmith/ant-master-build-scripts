e0b9d9cf7ddcd71635e381447028438f96594e84
douglas.bullard <douglas.bullard@gmail.com>
2012-07-09 05:42:19 +0000

    Refactoring stuff, rebuilding core objects via TDD.

--- src/main/java/com/nurflugel/buildtasks/todo/BadParsingException.java
+++ src/main/java/com/nurflugel/buildtasks/todo/BadParsingException.java
@@ -3,6 +3,8 @@ package com.nurflugel.buildtasks.todo;
 /** Created with IntelliJ IDEA. User: dbulla Date: 7/5/12 Time: 2:51 PM To change this template use File | Settings | File Templates. */
 public class BadParsingException extends Exception
 {
+  private static final long serialVersionUID = 1359466178008188147L;
+
   public BadParsingException(String message)
   {
     super(message);
